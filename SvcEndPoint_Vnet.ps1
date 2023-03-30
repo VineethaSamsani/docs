2ff7b93d7c945050ccdd8346a6ce15aa865b1fbc
VineethaSamsani <129390258+VineethaSamsani@users.noreply.github.com>
2023-03-30 16:55:14 +0530

    Add files via upload

---
+++ SvcEndPoint_Vnet.ps1
@@ -0,0 +1,14 @@
+﻿Connect-AzAccount -SubscriptionId 1505263f-be1f-4c0c-ba25-fcc8b91018ca
+
+$resourceGroupName = "AKSVSSFRG"
+$vnetName = "vnetscript"
+$subnetName = "subnet1"
+$subnetPrefix = "10.0.0.0/24"
+$serviceEndpoint = "Microsoft.ServiceBus"
+
+Get-AzVirtualNetwork `
+-ResourceGroupName $resourceGroupName `
+-Name $vnetName | Set-AzVirtualNetworkSubnetConfig `
+-Name $subnetName `
+-AddressPrefix $subnetPrefix `
+-ServiceEndpoint $serviceEndpoint | Set-AzVirtualNetwork
