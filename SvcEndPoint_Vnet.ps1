f2e108a7b9c058d4cf88e34c69fc9e245218e479
VineethaSamsani <129390258+VineethaSamsani@users.noreply.github.com>
2023-03-30 18:12:53 +0530

    ADDING UPDATED FILE

--- SvcEndPoint_Vnet.ps1
+++ SvcEndPoint_Vnet.ps1
@@ -1,4 +1,4 @@
-﻿Connect-AzAccount -SubscriptionId 1505263f-be1f-4c0c-ba25-fcc8b91018ca
+﻿Connect-AzAccount -SubscriptionId "1505263f-be1f-4c0c-ba25-fcc8b91018ca" -TenantId "e0793d39-0939-496d-b129-198edd916feb"
 
 $resourceGroupName = "AKSVSSFRG"
 $vnetName = "vnetscript"
@@ -6,9 +6,5 @@ $subnetName = "subnet1"
 $subnetPrefix = "10.0.0.0/24"
 $serviceEndpoint = "Microsoft.ServiceBus"
 
-Get-AzVirtualNetwork `
--ResourceGroupName $resourceGroupName `
--Name $vnetName | Set-AzVirtualNetworkSubnetConfig `
--Name $subnetName `
--AddressPrefix $subnetPrefix `
--ServiceEndpoint $serviceEndpoint | Set-AzVirtualNetwork
+Get-AzVirtualNetwork -ResourceGroupName $resourceGroupName -Name $vnetName | Set-AzVirtualNetworkSubnetConfig -Name $subnetName -AddressPrefix $subnetPrefix -ServiceEndpoint $serviceEndpoint | Set-AzVirtualNetwork
+
